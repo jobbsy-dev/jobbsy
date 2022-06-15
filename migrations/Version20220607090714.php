6369a32d721f13d5ab5f52b07ca5c7e03c604edc
Quentin Dequippe <quentin@dequippe.tech>
2022-06-15 15:58:19 +0200

    Update database config Docker and env

--- migrations/Version20220607090714.php
+++ migrations/Version20220607090714.php
@@ -7,16 +7,8 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20220607090714 extends AbstractMigration
 {
-    public function getDescription(): string
-    {
-        return '';
-    }
-
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
