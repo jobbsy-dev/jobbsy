8eabc6489173c26d758dfcc3ed5a53563fb59158
Quentin Dequippe <quentin@dequippe.tech>
2022-07-08 11:02:58 +0200

    Change Sentry config and fix deployment bug

--- config/packages/sentry.php
+++ config/packages/sentry.php
@@ -2,9 +2,26 @@
 
 declare(strict_types=1);
 
+use Sentry\Integration\IgnoreErrorsIntegration;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Config\SentryConfig;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\env;
 
-return static function (SentryConfig $config): void {
+return static function (SentryConfig $config, ContainerConfigurator $containerConfigurator): void {
     $config->dsn(env('SENTRY_DSN'));
+    $config->options()->integrations([IgnoreErrorsIntegration::class]);
+
+    $services = $containerConfigurator->services();
+
+    $services->set(IgnoreErrorsIntegration::class)
+        ->arg('$options', [
+            'ignore_exceptions' => [
+                NotFoundHttpException::class,
+                AccessDeniedException::class,
+                MethodNotAllowedHttpException::class
+            ]
+        ]);
 };
