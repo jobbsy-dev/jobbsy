539fe617887f558ec33821965252e77d6d90aa44
Quentin Dequippe <quentin@dequippe.tech>
2022-10-21 14:36:31 +0200

    run cs fixer

--- src/Controller/Admin/JobCrudController.php
+++ src/Controller/Admin/JobCrudController.php
@@ -103,6 +103,6 @@ class JobCrudController extends AbstractCrudController
             ->add('organization')
             ->add('employmentType')
             ->add('pinnedUntil')
-            ;
+        ;
     }
 }
