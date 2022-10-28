5c14633f91268a74411db44e57164ed52c25b06e
Quentin Dequippe <quentin@dequippe.tech>
2022-10-28 17:04:25 +0200

    feat: blog (#147)

---
+++ migrations/Version20221028130653.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221028130653 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE post (id CHAR(36) NOT NULL --(DC2Type:uuid)
+        , title VARCHAR(255) NOT NULL, slug VARCHAR(255) NOT NULL, summary VARCHAR(255) NOT NULL, content CLOB NOT NULL, published_at DATETIME DEFAULT NULL --(DC2Type:datetime_immutable)
+        , created_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
+        , PRIMARY KEY(id))');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE post');
+    }
+}
---
+++ src/Controller/BlogController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Repository\JobRepository;
+use App\Repository\PostRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    public function __construct(private readonly PostRepository $postRepository, private readonly JobRepository $jobRepository)
+    {
+    }
+
+    #[Route('/blog', name: 'blog_index', methods: ['GET'])]
+    public function index(Request $request, PaginatorInterface $paginator): Response
+    {
+        $queryBuilder = $this->postRepository->createQueryBuilderPublishedPosts();
+
+        $pagination = $paginator->paginate(
+            $queryBuilder,
+            $request->query->getInt('page', 1),
+            20
+        );
+
+        return $this->render('blog/index.html.twig', [
+            'pagination' => $pagination,
+        ]);
+    }
+
+    #[Route('/blog/{slug}', name: 'blog_post', methods: ['GET'])]
+    public function post(Post $post): Response
+    {
+        return $this->render('blog/show.html.twig', [
+            'post' => $post,
+            'latestJobs' => $this->jobRepository->findLastJobs(5),
+        ]);
+    }
+}
