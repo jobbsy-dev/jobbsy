4acc53bbe686fe16ee9898c877dce39ba487eb1b
Quentin Dequippe <quentin@dequippe.tech>
2022-11-03 10:27:57 +0100

    update migration

--- migrations/Version20221103092448.php
+++ migrations/Version20221103092448.php
@@ -7,11 +7,10 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20221102203238 extends AbstractMigration
+final class Version20221103092448 extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE article (id CHAR(36) NOT NULL --(DC2Type:uuid)
         , feed_id CHAR(36) NOT NULL --(DC2Type:uuid)
         , title VARCHAR(255) NOT NULL, link VARCHAR(255) NOT NULL, description CLOB NOT NULL, created_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
@@ -21,12 +20,12 @@ final class Version20221102203238 extends AbstractMigration
         $this->addSql('CREATE INDEX IDX_23A0E6651A5BC03 ON article (feed_id)');
         $this->addSql('CREATE TABLE feed (id CHAR(36) NOT NULL --(DC2Type:uuid)
         , url VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
-        , type VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        , type VARCHAR(255) NOT NULL, image_name VARCHAR(255) DEFAULT NULL, image_size INTEGER DEFAULT NULL, updated_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
+        , image_url VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE article');
         $this->addSql('DROP TABLE feed');
     }
