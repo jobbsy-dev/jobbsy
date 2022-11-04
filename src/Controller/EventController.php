ecedf43e57057c1d383e31821ae99629d62c6e86
Quentin Dequippe <quentin@dequippe.tech>
2022-11-04 09:46:33 +0100

    Add ref on urls (#167)
    * Add ref on urls
    
    * fix test

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,19 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\Event;
 use App\Repository\EventRepository;
+use League\Uri\Uri;
+use League\Uri\UriModifier;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EventController extends AbstractController
 {
-    #[Route('/events', name: 'app_event_index', methods: ['GET'])]
+    #[Route('/events', name: 'event_index', methods: ['GET'])]
     public function index(EventRepository $eventRepository): Response
     {
         return $this->render('event/index.html.twig', [
             'upcomingEvents' => $eventRepository->findUpcomingEvents(),
             'pastEvents' => $eventRepository->findPastEvents(),
         ]);
+    }
+
+    #[Route('/events/{id}', name: 'event_article', methods: ['GET'])]
+    public function event(Event $event): RedirectResponse
+    {
+        $uri = Uri::createFromString($event->getUrl());
+        $uri = UriModifier::appendQuery($uri, 'ref=jobbsy');
+
+        return $this->redirect($uri);
     }
 }
