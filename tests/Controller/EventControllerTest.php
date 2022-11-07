303fdb03a5b3dc58d0ae1aeede0d68fc966cdb17
Quentin Dequippe <quentin@dequippe.tech>
2022-11-07 14:21:18 +0100

    update pages titles (#173)

--- tests/Controller/EventControllerTest.php
+++ tests/Controller/EventControllerTest.php
@@ -17,7 +17,7 @@ class EventControllerTest extends WebTestCase
         $client->request('GET', '/events');
 
         self::assertResponseIsSuccessful();
-        self::assertPageTitleContains('Events');
+        self::assertPageTitleContains('Symfony conferences and events');
 
         self::assertSelectorTextContains('h1', 'Upcoming Symfony conferences and events');
         self::assertSelectorTextContains('h2', 'Upcoming events & meetups');
