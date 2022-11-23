fbc7a2f22663ddba82995d6f7e5f3a1ed297b022
Quentin Dequippe <quentin@dequippe.tech>
2022-11-23 17:37:05 +0100

    update routing recipe

--- config/routes.php
+++ config/routes.php
@@ -5,7 +5,5 @@ declare(strict_types=1);
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return static function (RoutingConfigurator $routingConfigurator): void {
-    $routingConfigurator->import('../src/Controller/', 'annotation');
-
-    $routingConfigurator->import('../src/Kernel.php', 'annotation');
+    $routingConfigurator->import('../src/Controller/', 'attribute');
 };
