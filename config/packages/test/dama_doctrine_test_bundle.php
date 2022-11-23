c5b3beea1d082919d1bf6ae53f64336df1e1d019
Quentin Dequippe <quentin@dequippe.tech>
2022-11-23 17:20:52 +0100

    update dama doctrine recipe

--- config/packages/test/dama_doctrine_test_bundle.php
+++ config/packages/test/dama_doctrine_test_bundle.php
@@ -8,6 +8,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->extension('dama_doctrine_test', [
         'enable_static_connection' => true,
         'enable_static_meta_data_cache' => true,
-        'enable_static_query_cache' => true
+        'enable_static_query_cache' => true,
     ]);
 };
