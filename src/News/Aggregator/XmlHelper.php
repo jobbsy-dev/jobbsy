b1664b89107d86955afc6d23fb3d1ad681d7276f
Quentin Dequippe <quentin@dequippe.tech>
2023-04-06 08:44:33 +0400

    apply rector

--- src/News/Aggregator/XmlHelper.php
+++ src/News/Aggregator/XmlHelper.php
@@ -7,12 +7,15 @@ final class XmlHelper
     public static function getNodeValue(\DOMXPath $xpath, $query, \DOMNode $element = null, $index = 0): mixed
     {
         $nodeList = $xpath->query($query, $element);
-
-        if ($nodeList->length > 0 && $index <= $nodeList->length) {
-            return self::sanitizeValue($nodeList->item($index)->nodeValue);
+        if ($nodeList->length <= 0) {
+            return null;
         }
 
-        return null;
+        if ($index > $nodeList->length) {
+            return null;
+        }
+
+        return self::sanitizeValue($nodeList->item($index)->nodeValue);
     }
 
     private static function sanitizeValue($value): mixed
