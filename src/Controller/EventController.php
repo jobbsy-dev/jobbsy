d84641b493e7bd4e8743d94a14ba2d14df47bf99
Quentin Dequippe <quentin@dequippe.tech>
2023-11-30 13:57:05 +0400

    fix code style

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -7,7 +7,6 @@ use App\Repository\CommunityEvent\EventRepository;
 use League\Uri\Modifier;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\Cache;
 use Symfony\Component\Routing\Annotation\Route;
