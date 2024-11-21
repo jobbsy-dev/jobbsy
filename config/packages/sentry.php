e95f70178e3f1b7ecd7d2a45e97d05f3967ed085
Quentin Dequippe <quentin@dequippe.tech>
2024-11-21 10:34:36 +0400

    ignore exceptions sentry

--- config/packages/sentry.php
+++ config/packages/sentry.php
@@ -3,6 +3,9 @@
 declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\env;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -11,6 +14,11 @@ return static function (ContainerConfigurator $containerConfigurator): void {
             'dsn' => env('SENTRY_DSN'),
             'register_error_listener' => false,
             'register_error_handler' => false,
+            'ignore_exceptions' => [
+                NotFoundHttpException::class,
+                BadRequestException::class,
+                MethodNotAllowedHttpException::class,
+            ],
         ]);
     }
 };
